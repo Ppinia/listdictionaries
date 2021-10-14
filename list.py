ca74aa0c1a46eeb7f0932edf613f483658b7207d
Marta Stimec <marta.dovic@gmail.com>
2021-10-14 15:46:16 +0200

    init commit

---
+++ list.py
@@ -0,0 +1,33 @@
+import json
+import random
+import datetime
+
+current_time = datetime.datetime.now()
+print(current_time)
+
+secret = random.randint(1, 10)
+attempts = 0
+
+with open("score_list.json", "r") as score_file:
+    score_list = json.loads(score_file.read())
+    print("Top scores: " + str(score_list))
+
+for score_dict in score_list:
+    print(str(score_dict["attempts"]) + " attempts, date: " + score_dict.get("date"))
+while True:
+    guess = int(input("enter your answer, what is the secret number 1-10: "))
+    attempts += 1
+
+    if guess == secret:
+        score_list.append({"attempts": attempts, "date": str(datetime.datetime.now())})
+
+        with open("score_list.json", "w") as score_file:
+            score_file.write(json.dumps(score_list))
+
+        print("congrats")
+        print("attempts needed: " + str(attempts))
+        break
+    elif guess > secret:
+            print("Your guess is not correct... try something smaller")
+    elif guess < secret:
+            print("Your guess is not correct... try something bigger")
